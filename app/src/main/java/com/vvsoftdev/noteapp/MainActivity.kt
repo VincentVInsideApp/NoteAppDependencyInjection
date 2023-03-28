f3d583f6960353d60bd58392dd0b107ee698007e
VincentVInsideApp <vincent.vigie@insideapp.fr>
2023-03-28 15:02:44 +0200

    init

---
+++ app/src/main/java/com/vvsoftdev/noteapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.vvsoftdev.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.vvsoftdev.noteapp.theme.CleanArchitectureNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanArchitectureNoteAppTheme {
+                
+            }
+        }
+    }
+}
